a4f28cbeeab4069df8dbcfc387aedffcf4d75f55
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2022-07-10 17:26:32 +0200

    added requirements.txt

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app import app
-from app.database import *
-with app.app_context():
-    db.create_all()
+# from app.database import *
+# with app.app_context():
+#     db.create_all()
 
 app.run(debug=True)
